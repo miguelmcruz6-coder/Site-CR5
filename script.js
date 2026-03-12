d99ed28abaff7c07c35c84364c05c7bc1933f2ff
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2026-03-12 11:37:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarMenu(){
+
+  const menu = document.getElementById("abas");
+
+  if(menu.style.display === "block"){
+      menu.style.display = "none";
+  }else{
+      menu.style.display = "block";
+  }
+
+}
